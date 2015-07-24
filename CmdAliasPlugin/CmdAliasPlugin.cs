3f1815bd07297ee6ed6827c3798eec4d6b91746e
Tyler Watson <tyler@tw.id.au>
2015-07-24 13:09:40 +1000

    Update 21: Tick auxiliary plugins to 1.20

--- CmdAliasPlugin/CmdAliasPlugin.cs
+++ CmdAliasPlugin/CmdAliasPlugin.cs
@@ -17,7 +17,7 @@ namespace Wolfje.Plugins.SEconomy.CmdAliasModule {
 	/// <summary>
 	/// Provides command aliases that can cost money to execute in SEconomy.
 	/// </summary>
-	[ApiVersion(1, 19)]
+	[ApiVersion(1, 20)]
 	public class CmdAliasPlugin : TerrariaPlugin {
 		protected static CmdAlias aliasCmdInstance;
 
